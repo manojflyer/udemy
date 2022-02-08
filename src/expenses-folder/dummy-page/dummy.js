a0437938400c213fb5d5c02908d2d50f15bbcfce
manoj.surabathulla <manoj.surabathulla@flyerssoft.com>
2022-02-08 15:28:09 +0530

    dummy changes

--- src/expenses-folder/dummy-page/dummy.js
+++ src/expenses-folder/dummy-page/dummy.js
@@ -9,3 +9,15 @@ const Dummy = () => {
 };
 
 export default Dummy;
+import React from 'react';
+import './dummy.scss'
+
+const Dummy = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Dummy;
